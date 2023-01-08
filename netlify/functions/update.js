4b6c0719e243c1c9e72e2fd75ac9208caca89678
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-01-07 19:46:38 -0600

    Add error handling in axios POST

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -3,14 +3,18 @@ const axios = require("axios").default;
 exports.handler = async (event) => {
   console.log("Received an update from Telegram!", event.body);
 
-  await axios.post(
-    `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
-    {
-      chat_id: JSON.parse(event.body).message.chat.id,
-      text: "I got your message!",
-      reply_markup: [["First button", "Second button"]],
-    }
-  );
+  await axios
+    .post(
+      `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+      {
+        chat_id: JSON.parse(event.body).message.chat.id,
+        text: "I got your message!",
+        reply_markup: [["First button", "Second button"]],
+      }
+    )
+    .catch(function (error) {
+      console.log(error.toJSON());
+    });
 
   return { statusCode: 200 };
 };
