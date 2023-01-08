839f3c852f21df0574f69116c3f2f9abc9a63ff4
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-01-08 13:04:49 -0600

    Remove src folder

--- netlify/functions/updates.js
+++ netlify/functions/updates.js
@@ -6,23 +6,23 @@ exports.updates = async (e) => {
   // return res;
   console.log("Received an update from Telegram!", e.body);
 
-  await axios
-    .post(
-      `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
-      {
-        chat_id: JSON.parse(e.body).message.chat.id,
-        text: "I got your message!",
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: "Button", callback_data: "callback string" }],
-          ],
-        },
-      }
-    )
-    .catch((error) => {
-      console.log(error.toJSON());
-      return { statusCode: 500 };
-    });
+  // await axios
+  //   .post(
+  //     `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+  //     {
+  //       chat_id: JSON.parse(e.body).message.chat.id,
+  //       text: "I got your message!",
+  //       reply_markup: {
+  //         inline_keyboard: [
+  //           [{ text: "Button", callback_data: "callback string" }],
+  //         ],
+  //       },
+  //     }
+  //   )
+  //   .catch((error) => {
+  //     console.log(error.toJSON());
+  //     return { statusCode: 500 };
+  //   });
 
-  return { statusCode: 200 };
+  // return { statusCode: 200 };
 };
